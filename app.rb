cb94761900d2a8f21e622e8477111f015fda1acb
Nicole Dow <nikki.dow71@gmail.com>
2019-08-28 01:02:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,22 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
+    
 
-    # code other routes/actions here
+    get '/' do
+      erb :root 
+   end
+   
+   get '/new' do 
+     erb :new  
+   end
+   
+   post '/pirates' do
+     #binding.pry
+     @new_pirate = Pirate.new(params["pirate"]["name"], params["pirate"]["weight"], params["pirate"]["height"])
+     @ships_array = params["pirate"]["ships"]
+     erb :show
+  end
 
   end
 end
